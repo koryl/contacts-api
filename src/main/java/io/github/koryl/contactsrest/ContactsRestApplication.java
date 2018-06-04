fe128edf8273bc0afefc443f058140c523eeb451
Adrian Koryl <adrian.koryl@gmail.com>
2018-06-04 09:57:42 +0200

    Initialization of project

---
+++ src/main/java/io/github/koryl/contactsrest/ContactsRestApplication.java
@@ -0,0 +1,12 @@
+package io.github.koryl.contactsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsRestApplication.class, args);
+	}
+}
